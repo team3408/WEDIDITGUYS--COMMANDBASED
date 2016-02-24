f08be113783a2c7d37bf5e666d80160b3b44b7ce
Alexhb61 <yruaskingy@gmail.com>
2016-02-23 20:17:59 -0800

    added drivesetdistance stuff from p if loop branch

--- betterImportSteveSprocky/src/Commands/DriveSetDistance.cpp
+++ betterImportSteveSprocky/src/Commands/DriveSetDistance.cpp
@@ -2,6 +2,7 @@
 
 #include "Commands/Subsystem.h"
 #include "../Robot.h"
+#include <cmath>
 
 DriveSetDistance::DriveSetDistance(double Distance)
 {
@@ -14,19 +15,64 @@ DriveSetDistance::DriveSetDistance(double Distance)
 void DriveSetDistance::Initialize()
 {
 
+
 }
+
+	float midPower = 0.66;
+   float leftPower;
+   float rightPower;
+   float currentDistance;
+   int currentEncCountLeft;
+   int currentEncCountRight;
+
+   int overshootNumber = 100;
+
+
+   float targetDistance;
+   const float encoderCountsPerRotation = 360;
+   const float diameter = 7.6;						// 7.6 inches is the diameter of our robot
+   const float circumference = diameter * 3.14159;
+   float rotations = targetDistance / circumference;
+   float EncErrorLeft;
+   int initialEncCountLeft = Robot::drive->Encoders();
+   float targetEncoderCounts = encoderCountsPerRotation * rotations + initialEncCountLeft;
+   float left;
+   float right;
+	 double gain = 0.01;
+	 float power;
 
 // Called repeatedly when this Command is scheduled to run
 void DriveSetDistance::Execute()
 {
-	Robot::drive -> TwoAxis(1, 2);
+
+    currentEncCountLeft = Robot::drive ->Encoders();
+
+    EncErrorLeft = targetEncoderCounts - currentEncCountLeft;
+
+
+	power = midPower + (EncErrorLeft * gain);
+
+	if (power > 1) {
+
+		power = 1;
+
+	} else if (power < -1){
+
+		power = -1;
+
+	}
+
+	left = power;
+	right = power;
+
+	Robot::drive -> TwoAxis(left, right);
 
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool DriveSetDistance::IsFinished()
 {
-	return false;
+	return (currentEncCountLeft > targetEncoderCounts - overshootNumber && currentEncCountLeft < targetEncoderCounts + overshootNumber);
 }
 
 // Called once after isFinished returns true
